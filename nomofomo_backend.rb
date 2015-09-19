2203e4962218ab66800124295c4d5f5da9f34555
Ellen Sebastian <ellens2@stanford.edu>
2015-09-19 14:48:53 -0400

    change datastore to reflect api requirements. create User & Event classes

--- nomofomo_backend.rb
+++ nomofomo_backend.rb
@@ -17,19 +17,48 @@ options "*" do
   200
 end
 
-# map from event id -> [name, minAttendance, hostUserId, yelpid]
-events = {1 => ["dinner @ the Lockhart", 3, 5, 13246]}
+# map of userid -> user
+users = {}
 
-# map from hashed fb id -> array of their favorited event ids
-user_favorites = {}
+# map of eventid -> event
+events = {}
+
+# map from hashed fb id -> array of their created event ids
+user_created_events = {}
+
+# map from hashed fb id -> array of their interested (right swiped) event ids
+user_interested_events = {}
 
 # map from hashed fb id -> array of their left swiped (dismissed) event ids
-user_left_swipes = {}
+user_disliked_events = {}
 
 # map from hashed fb id -> array of their confirmed event ids
 user_confirmed_events = {}
 
+class User
+   def initialize(id, name, picture)
+      @user_id=id
+      @name=name
+      @picture=picture
+   end
+end
 
+class Event
+	def initialize(creator, name, description, lat, lng, start_time, 
+		duration, picture, metadata, interested_users, confirmed_users)
+		@creator=creator
+		@name=name
+		@description=description
+		@lat=lat
+		@lng=lng
+		@start_time=start_time
+		@duration=duration
+		@picture=picture
+		@metadata=metadata
+		@interested_users=interested_users
+		@confirmed_users=confirmed_users
+	end
+end
 
 get '/hello' do
   url = params[:url]
